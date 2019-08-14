e055ffafa3c876733101fcb97ceada4e6f3c269e
Ronald Portier <ronald@therp.nl>
2019-08-14 14:47:28 +0200

    [IMP] website_menu_permissions. Updated headers.

--- website_menu_permission/models/website_menu.py
+++ website_menu_permission/models/website_menu.py
@@ -1,13 +1,11 @@
-# Copyright 2017 Simone Orsi
-# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+# Copyright 2017 Simone Orsi.
+# License LGPL-3.0 or later (https://www.gnu.org/licenses/lgpl).
 from odoo import fields, models
 
 
 class WebsiteMenu(models.Model):
     _inherit = 'website.menu'
 
-    # TODO: when we'll have an advanced form for editing groups
-    # we should add an handler to update flags (like `_update_menu_groups`)
     group_ids = fields.Many2many(
         comodel_name='res.groups',
     )
