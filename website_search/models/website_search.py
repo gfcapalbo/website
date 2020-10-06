d8335799259de3a5e67f03e9ea63db7ac1cab3e7
Giovanni Francesco Capalbo <giovanni@therp.nl>
2020-10-06 17:20:50 +0200

    [FIX]

--- website_search/models/website_search.py
+++ website_search/models/website_search.py
@@ -34,33 +34,42 @@ class WebsiteSearch(models.TransientModel):
         # specific search method for a type of resource
         # must create records of type website.search.result  and add them
         # to result_ids of this search.
-        self.env.cr.execute(
-            """select
-                id from (
-                    select
-                           id, name,
-                           lower (array_to_string(
-                                xpath(
-                                    '//*//text()', arch_db::xml), '\n'
-                                )) as view_text
-                    from ir_ui_view
-                    where type='qweb' and page='t') as table_text
-                where table_text.view_text ilike %s or name ilike %s""",
-            ('%' + (self.name or '') + '%', '%' + (self.name or '') + '%', )
-        )
-        ids = [_id for _id, in self.env.cr.fetchall()]
-
-        for view in self.env['ir.ui.view'].search([('id', 'in', ids)]):
-            self.env['website.search.result'].create({
-                'res_model': 'ir.ui.view',
-                'search_id': self.id,
-                'type': 'page',
-                'res_id': view.id,
-                'link': '/page/%s' % view.key,
-                'name': view.name or view.key,
-                # give higher rank if search text is in page title
-                'rank': 1 and self.name.lower() in view.name.lower() or 2,
-            })
+        import pudb
+        pudb.set_trace()
+        pages = self.env['ir.ui.view'].search([
+            ('page_ids', '=', True),
+            ('website_id', '=' , self.env.context.get('website_id'),
+        ])
+        if self.env.context.get("website.id") and pages:
+            self.env.cr.execute(
+                """select
+                    id from (
+                        select
+                               id, website_id, name,
+                               lower (array_to_string(
+                                    xpath(
+                                        '//*//text()', arch_db::xml), '\n'
+                                    )) as view_text
+                        from ir_ui_view
+                        where type='qweb' and id in %s and
+                    ) as table_text
+                    where table_text.view_text ilike %s or name ilike %s""",
+                (pages.ids,
+                 '%' + (self.name or '') + '%',
+                 '%' + (self.name or '') + '%', )
+            )
+            ids = [_id for _id, in self.env.cr.fetchall()]
+            for view in self.env['ir.ui.view'].search([('id', 'in', ids)]):
+                self.env['website.search.result'].create({
+                    'res_model': 'ir.ui.view',
+                    'search_id': self.id,
+                    'type': 'page',
+                    'res_id': view.id,
+                    'link': '/page/%s' % view.key,
+                    'name': view.name or view.key,
+                    # give higher rank if search text is in page title
+                    'rank': 1 and self.name.lower() in view.name.lower() or 2,
+                })
 
     def _do_search_blog_post(self):
         # will have to verify if blog installed, then search in content and
