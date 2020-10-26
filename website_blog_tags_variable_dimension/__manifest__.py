c08637155eaa404acce5c751c1049c83d39f07d9
Giovanni Francesco Capalbo <giovanni@therp.nl>
2020-10-26 16:26:52 +0100

    [ADD] website_blog_tags_variable_dimension v14

---
+++ website_blog_tags_variable_dimension/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+# © 2016 Therp BV <http://therp.nl>
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl.html).
+{
+    "name": "Blog tags cloud variable dimension",
+    "version": "14.0.1.0.0",
+    "author": "Therp BV,Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "category": "Website",
+    "summary": "Adds an option to add a tag cloud to the blog right column",
+    "depends": [
+        'website_blog',
+    ],
+    "data": [
+        'views/templates.xml',
+    ],
+    "installable": True,
+    "application": False,
+}
